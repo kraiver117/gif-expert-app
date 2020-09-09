170c18c95cd29e27b28d9ea350eec1126e7d6c90
Angel Gutierrez <gutierrezramosjoseangel@gmail.com>
2020-09-09 01:19:59 -0500

    Docs added

---
+++ docs/precache-manifest.528b59ee353788b7a68ad515efb46199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908c0529b2ff752ef50bff5ad09e2098",
+    "url": "/index.html"
+  },
+  {
+    "revision": "553144b7dcaf82b1b922",
+    "url": "/static/css/main.ac81b1e3.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553144b7dcaf82b1b922",
+    "url": "/static/js/main.c92d01a8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
